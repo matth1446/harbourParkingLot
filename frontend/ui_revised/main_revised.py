a45540e605de5e99ec11900e26b54c49f2c4fbf2
Christopher Wildzeisz <2110859015@fh-burgenland.at>
2023-05-24 15:49:49 +0200

    changed visualization code into functions

--- frontend/ui_revised/main_revised.py
+++ frontend/ui_revised/main_revised.py
@@ -24,7 +24,7 @@ class MainWindow(QMainWindow):
         # Create an instance of the generated UI class
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #self.setGeometry(100, 100, 800, 900)
+        # self.setGeometry(100, 100, 800, 900)
 
         # Add functionality to grid buttons
         for row in range(self.ui.gridLayout_roads.rowCount()):
@@ -49,40 +49,98 @@ class MainWindow(QMainWindow):
 
         # Add functionality for plotting a chart to the output panel
         button_retrieve_results = self.ui.but_retrieve_res
-        button_retrieve_results.clicked.connect(self.create_charts)
+        button_retrieve_results.clicked.connect(self.retrieve_simulation_results)
 
-    def create_charts(self):
-        gates = [1, 2, 3, 4]
-        data_y1 = [10, 20, 30, 40]
-        data_y2 = [20, 30, 40, 50]
-        data_x = np.arange(len(gates))
-        # Creating the Figure container
+    def retrieve_simulation_results(self):
+        # gather all data from the simulation (inputs, results)
+
+        # generate charts for the simulation
+
+        ax, fig = self.create_serviced_vehicles_chart()
+
+        # create chart two
+        cars = 35
+        trucks = 25
+        ticket_price = 7.5
+        percentage_for_parking = 0.05
+        gates = 4
+        gates_open = 2  # hours
+        payment = 15  # hours
+
+        ticket_parking = ticket_price * percentage_for_parking
+
+        income = (cars * ticket_parking) + (trucks * (ticket_parking * 1.5))
+        # Expenses = price for each employee per gate (excl. sel-check-in) and hours
+        expenses = (gates * (gates_open * payment))
+
+        x_labels = ["Income", "Expenses"]
+        y_axis = [income, expenses]
+        data_x = np.arange(len(x_labels))
+        barlist = ax.bar(data_x, y_axis)
+        barlist[1].set_color('red')
+        ax.set_ylabel('Euro')
+        ax.set_title('Overview of Income and Expenses')
+        ax.set_xticks(data_x, x_labels)
+
+        fig.savefig('img/chart_0_1.png')
+        ax.clear()
+
+        # create chart three
+        avg_car = 23
+        avg_truck = 45
         fig = mpl.figure(figsize=(5, 5))
-        # Creating the axis
         ax = fig.add_subplot()
-        # Line plot command
-        ax.bar(data_x, data_y1, label='Vehicles handled')
-        ax.bar(data_x, data_y2, bottom=data_y1, label='Vehicles waiting')
-        ax.set_ylabel('Vehicles')
+        x_labels = ["Car", "Truck"]
+        y_axis = [avg_car, avg_truck]
+        data_x = np.arange(len(x_labels))
+        ax.bar(data_x, y_axis)
+        ax.set_ylabel('Minutes')
+        ax.set_title('Average Waiting Time in Minutes')
+        ax.set_xticks(data_x, x_labels)
+
+        fig.savefig('img/chart_1_0.png')
+        ax.clear()
+
+        # create chart four
+        emission_car = 19.1
+        emission_truck = 45.56
+        gates = [1, 2, 3, 4]
+        waiting_time_cars = [50, 30, 58, 48]
+        waiting_time_trucks = [60, 12, 0, 70]
+
+        # to add emission
+        for i in range(len(waiting_time_cars)):
+            waiting_time_cars[i] = waiting_time_cars[i] * emission_car
+
+        for i in range(len(waiting_time_trucks)):
+            waiting_time_trucks[i] = waiting_time_trucks[i] * emission_truck
+
+        print(waiting_time_cars)
+        data_x = np.arange(len(gates))
+        width = 0.25
+        ax.bar(data_x - width / 2, waiting_time_cars, width, label='Cars')
+        ax.bar(data_x + width / 2, waiting_time_trucks, width, label='Trucks')
+        ax.set_ylabel('co¹ emission in gramm per min')
         ax.set_xlabel('Gates')
-        ax.set_title('Handled Vehicles per Gate')
+        ax.set_title('Produced CO² Emission during Waiting Time')
+        ax.set_xticks(data_x, gates)
+        ax.grid()
         ax.legend()
 
-        ax.set_xticks(data_x, gates)
-        fig.savefig('img/chart_0_0.png')
+        fig.savefig('img/chart_1_1.png')
 
         # assigning charts to the placeholders
 
         chart_img_0_0 = QPixmap('./img/chart_0_0.png')
         chart_img_0_0 = chart_img_0_0.scaled(500, 500)
 
-        chart_img_0_1 = QPixmap('./img/chart_0_0.png')
+        chart_img_0_1 = QPixmap('./img/chart_0_1.png')
         chart_img_0_1 = chart_img_0_1.scaled(500, 500)
 
-        chart_img_1_0 = QPixmap('./img/chart_0_0.png')
+        chart_img_1_0 = QPixmap('./img/chart_1_0.png')
         chart_img_1_0 = chart_img_1_0.scaled(500, 500)
 
-        chart_img_1_1 = QPixmap('./img/chart_0_0.png')
+        chart_img_1_1 = QPixmap('./img/chart_1_1.png')
         chart_img_1_1 = chart_img_1_1.scaled(500, 500)
 
         chart_0_0_label = self.ui.img_out_0_0
@@ -104,6 +162,27 @@ class MainWindow(QMainWindow):
         lab_sim_text.setText("Simulation id: ")
         lab_sim_id.setText("0")
 
+    def create_serviced_vehicles_chart(self):
+        # create chart one
+        gates = [1, 2, 3, 4]
+        data_y1 = [10, 20, 30, 40]
+        data_y2 = [20, 30, 40, 50]
+        data_x = np.arange(len(gates))
+        # Creating the Figure container
+        fig = mpl.figure(figsize=(5, 5))
+        # Creating the axis
+        ax = fig.add_subplot()
+        # Line plot command
+        ax.bar(data_x, data_y1, label='Vehicles handled')
+        ax.bar(data_x, data_y2, bottom=data_y1, label='Vehicles waiting')
+        ax.set_ylabel('Vehicles')
+        ax.set_xlabel('Gates')
+        ax.set_title('Handled Vehicles per Gate')
+        ax.legend()
+        ax.set_xticks(data_x, gates)
+        fig.savefig('img/chart_0_0.png')
+        ax.clear()
+        return ax, fig
 
     def update_multiplier_text(self):
         slider_capacity_multiplier = self.ui.horSlider_capacity_multiplier
--- frontend/visualization/visualization.py
+++ frontend/visualization/visualization.py
@@ -1,112 +1,118 @@
 import matplotlib.pyplot as mpl
-import pandas as pd
 import numpy as np
 
-#chart 1 ---------------------------------------------------------------------------------------
-#Handled Vehicles per Gate
-#createing ficticious data to plot.
-gates = [1,2,3,4]
-data_y1 = [10, 20, 30, 40]
-data_y2 = [20, 30, 40, 50]
-data_x = np.arange(len(gates))
-#Creating the Figure container
-fig = mpl.figure(figsize=(5,5))
-#Creating the axis
-ax = fig.add_subplot()
-#Line plot command
-ax.bar(data_x, data_y1, label='Vehicles handled')
-ax.bar(data_x, data_y2, bottom = data_y1, label='Vehicles waiting')
-ax.set_ylabel('Vehicles')
-ax.set_xlabel('Gates')
-ax.set_title('Handled Vehicles per Gate')
-ax.legend()
 
-ax.set_xticks(data_x, gates)
-#ax.grid()
-mpl.show()
+def create_handled_vehicles_chart():
+    # chart 1 ---------------------------------------------------------------------------------------
+    # Handled Vehicles per Gate
+    # createing ficticious data to plot.
+    gates = [1, 2, 3, 4]
+    data_y1 = [10, 20, 30, 40]
+    data_y2 = [20, 30, 40, 50]
+    data_x = np.arange(len(gates))
+    # Creating the Figure container
+    fig = mpl.figure(figsize=(5, 5))
+    # Creating the axis
+    ax = fig.add_subplot()
+    # Line plot command
+    ax.bar(data_x, data_y1, label='Vehicles handled')
+    ax.bar(data_x, data_y2, bottom=data_y1, label='Vehicles waiting')
+    ax.set_ylabel('Vehicles')
+    ax.set_xlabel('Gates')
+    ax.set_title('Handled Vehicles per Gate')
+    ax.legend()
+
+    ax.set_xticks(data_x, gates)
+    # ax.grid()
+    # mpl.show()
 
 
-#chart 2 ---------------------------------------------------------------------------------------
-#Overview of Income and Expenses
-#Income = sold ticked
-cars = 35
-trucks = 25
-ticket_price = 7.5
-#how much is for the parking?
-# 5% for the pre-gate-parking
-percentage_for_parking = 0.05
-ticket_parking = ticket_price*percentage_for_parking
+def create_income_expenses_chart():
+    # chart 2 ---------------------------------------------------------------------------------------
+    # Overview of Income and Expenses
+    # Income = sold ticked
+    cars = 35
+    trucks = 25
+    ticket_price = 7.5
+    # how much is for the parking?
+    # 5% for the pre-gate-parking
+    percentage_for_parking = 0.05
+    ticket_parking = ticket_price * percentage_for_parking
 
-income = (cars*ticket_parking)+(trucks*(ticket_parking*1.5))
+    income = (cars * ticket_parking) + (trucks * (ticket_parking * 1.5))
 
-#Expenses = price for each employee per gate (excl. sel-check-in) and hours
+    # Expenses = price for each employee per gate (excl. sel-check-in) and hours
 
-gates = 4
-gates_open = 2 #hours
-#payment between 10 to 20 wuro
-payment = 15 #hours
+    gates = 4
+    gates_open = 2  # hours
+    # payment between 10 to 20 wuro
+    payment = 15  # hours
 
-expenses = (gates*(gates_open*payment))
+    expenses = (gates * (gates_open * payment))
 
-#chart
-fig = mpl.figure(figsize=(5,5))
-ax = fig.add_subplot()
-x_labels = ["Income","Expenses"]
-y_axis = [income, expenses]
-data_x = np.arange(len(x_labels))
-barlist = ax.bar(data_x, y_axis)
-barlist[1].set_color('red')
-ax.set_ylabel('Euro')
-ax.set_title('Overview of Income and Expenses')
-ax.set_xticks(data_x, x_labels)
-mpl.show()
+    # chart
+    fig = mpl.figure(figsize=(5, 5))
+    ax = fig.add_subplot()
+    x_labels = ["Income", "Expenses"]
+    y_axis = [income, expenses]
+    data_x = np.arange(len(x_labels))
+    barlist = ax.bar(data_x, y_axis)
+    barlist[1].set_color('red')
+    ax.set_ylabel('Euro')
+    ax.set_title('Overview of Income and Expenses')
+    ax.set_xticks(data_x, x_labels)
+    # mpl.show()
 
-#chart 3 ---------------------------------------------------------------------------------------
-#Average waiting time
-avg_car = 23
-avg_truck = 45
-fig = mpl.figure(figsize=(5,5))
-ax = fig.add_subplot()
-x_labels = ["Car","Truck"]
-y_axis = [avg_car, avg_truck]
-data_x = np.arange(len(x_labels))
-ax.bar(data_x, y_axis)
-ax.set_ylabel('Minutes')
-ax.set_title('Average Waiting Time in Minutes')
-ax.set_xticks(data_x, x_labels)
-mpl.show()
 
-#chart 4 ---------------------------------------------------------------------------------------
-#CO² Emission of Vehicles
-#Car: 19,1 Gramm CO² per min
-#Truck: 45,56 gramm CO² per min
-emission_car = 19.1
-emission_truck = 45.56
+def create_avg_waiting_chart():
+    # chart 3 ---------------------------------------------------------------------------------------
+    # Average waiting time
+    avg_car = 23
+    avg_truck = 45
+    fig = mpl.figure(figsize=(5, 5))
+    ax = fig.add_subplot()
+    x_labels = ["Car", "Truck"]
+    y_axis = [avg_car, avg_truck]
+    data_x = np.arange(len(x_labels))
+    ax.bar(data_x, y_axis)
+    ax.set_ylabel('Minutes')
+    ax.set_title('Average Waiting Time in Minutes')
+    ax.set_xticks(data_x, x_labels)
+    # mpl.show()
 
-fig = mpl.figure(figsize=(5,5))
-ax = fig.add_subplot()
-gates = [1,2,3,4]
 
-waiting_time_cars = [50,30,58,48]
-waiting_time_trucks = [60,12,0,70]
+def create_co2_emission_chart():
+    # chart 4 ---------------------------------------------------------------------------------------
+    # CO² Emission of Vehicles
+    # Car: 19,1 Gramm CO² per min
+    # Truck: 45,56 gramm CO² per min
+    emission_car = 19.1
+    emission_truck = 45.56
 
-#to add emission
-for i in range(len(waiting_time_cars)):
-    waiting_time_cars[i] = waiting_time_cars[i]*emission_car
+    fig = mpl.figure(figsize=(5, 5))
+    ax = fig.add_subplot()
+    gates = [1, 2, 3, 4]
 
-for i in range(len(waiting_time_trucks)):
-    waiting_time_trucks[i] = waiting_time_trucks[i] * emission_truck
+    waiting_time_cars = [50, 30, 58, 48]
+    waiting_time_trucks = [60, 12, 0, 70]
 
-print(waiting_time_cars)
-data_x = np.arange(len(gates))
-width = 0.25
-ax.bar(data_x - width/2, waiting_time_cars, width, label='Cars')
-ax.bar(data_x + width/2, waiting_time_trucks, width, label='Trucks')
-ax.set_ylabel('co¹ emission in gramm per min')
-ax.set_xlabel('Gates')
-ax.set_title('Produced CO² Emission during Waiting Time')
-ax.set_xticks(data_x, gates)
-ax.grid()
-ax.legend()
+    # to add emission
+    for i in range(len(waiting_time_cars)):
+        waiting_time_cars[i] = waiting_time_cars[i] * emission_car
 
-mpl.show()
+    for i in range(len(waiting_time_trucks)):
+        waiting_time_trucks[i] = waiting_time_trucks[i] * emission_truck
+
+    print(waiting_time_cars)
+    data_x = np.arange(len(gates))
+    width = 0.25
+    ax.bar(data_x - width / 2, waiting_time_cars, width, label='Cars')
+    ax.bar(data_x + width / 2, waiting_time_trucks, width, label='Trucks')
+    ax.set_ylabel('co¹ emission in gramm per min')
+    ax.set_xlabel('Gates')
+    ax.set_title('Produced CO² Emission during Waiting Time')
+    ax.set_xticks(data_x, gates)
+    ax.grid()
+    ax.legend()
+
+    mpl.show()
